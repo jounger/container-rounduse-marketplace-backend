26c5e488e3cb277d6b726f4c08069b85676d7520
jounger <nguyenvanan.12a8@gmail.com>
2020-05-29 16:09:59 +0700

    BACKEND PROJECT INIT

---
+++ src/main/java/com/crm/ContainerRounduseMarketplaceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerRounduseMarketplaceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerRounduseMarketplaceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/ContainerRounduseMarketplaceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerRounduseMarketplaceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

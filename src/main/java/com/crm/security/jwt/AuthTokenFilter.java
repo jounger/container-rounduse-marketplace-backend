96b7b0e90f62163b1137462fbacf88d92ed526f5
jounger <nguyenvanan.12a8@gmail.com>
2020-06-02 10:50:22 +0700

    CONFIG with vue auth plugin: refresh token & user

--- src/main/java/com/crm/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/crm/security/jwt/AuthTokenFilter.java
@@ -49,7 +49,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
     filterChain.doFilter(request, response);
   }
   
-  private String parseJwt(HttpServletRequest request) {
+  public static String parseJwt(HttpServletRequest request) {
     String headerAuth = request.getHeader("Authorization");
     if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
       return headerAuth.substring(7, headerAuth.length());

27f896650011b4e93ebf78d8b03334b3ab4199f7
minhkq1998 <khong.quangminh1@gmail.com>
2020-06-16 17:13:25 +0700

    getListConsignments

--- src/main/java/com/crm/controllers/ConsignmentController.java
+++ src/main/java/com/crm/controllers/ConsignmentController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class ConsignmentController {
   
   private ConsignmentService consignmentService;
   
+  @GetMapping("/")
   public ResponseEntity<?> getListConsignment(@Valid PaginationRequest request) {
     
     Page<Consignment> pages = consignmentService.getListConsignment(request);

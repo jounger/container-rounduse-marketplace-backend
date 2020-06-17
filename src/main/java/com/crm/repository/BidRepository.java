83ccb2a0135cd44ea6ab60ea22f68b208378bf39
Nguyen Van An <44369558+jounger@users.noreply.github.com>
2020-06-18 01:07:23 +0700

    Add forwarder and container mapper (#5)
    Co-authored-by: MinQ <nguyenquyenmnek57@gmail.com>

--- src/main/java/com/crm/repository/BidRepository.java
+++ src/main/java/com/crm/repository/BidRepository.java
@@ -3,9 +3,9 @@ package com.crm.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.crm.models.BiddingDocument;
+import com.crm.models.Bid;
 
 @Repository
-public interface ProposalRepository extends JpaRepository<BiddingDocument, Long>{
+public interface BidRepository extends JpaRepository<Bid, Long>{
 
 }

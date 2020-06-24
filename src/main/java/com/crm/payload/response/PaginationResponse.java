e6791b2081b52ebe46ba406cf8b9e3e3795b4fc2
Nguyen Van An <nguyenvanan.12a8@gmail.com>
2020-06-24 20:47:30 +0700

    ADD import.sql

--- src/main/java/com/crm/payload/response/PaginationResponse.java
+++ src/main/java/com/crm/payload/response/PaginationResponse.java
@@ -3,8 +3,6 @@ package com.crm.payload.response;
 import java.io.Serializable;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,19 +16,14 @@ public class PaginationResponse<E> implements Serializable{
 
   private static final long serialVersionUID = 1L;
 
-  @JsonProperty("page")
   private int pageNumber;
   
-  @JsonProperty("limit")
   private int pageSize;
   
-  @JsonProperty("total_elements")
   private long totalElements;
   
-  @JsonProperty("total_pages")
   private int totalPages;
   
-  @JsonProperty("data")
   private List<E> contents;
 
 }

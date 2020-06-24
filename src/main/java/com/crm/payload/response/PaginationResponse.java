03d4eefc35c4b3984c0a21442789a30001f88e09
Nguyen Van An <nguyenvanan.12a8@gmail.com>
2020-06-24 20:52:13 +0700

    Fix pagination response

--- src/main/java/com/crm/payload/response/PaginationResponse.java
+++ src/main/java/com/crm/payload/response/PaginationResponse.java
@@ -3,6 +3,8 @@ package com.crm.payload.response;
 import java.io.Serializable;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,14 +18,19 @@ public class PaginationResponse<E> implements Serializable{
 
   private static final long serialVersionUID = 1L;
 
+  @JsonProperty("page")
   private int pageNumber;
   
+  @JsonProperty("limit")
   private int pageSize;
   
+  @JsonProperty("totalElements")
   private long totalElements;
   
+  @JsonProperty("totalPages")
   private int totalPages;
   
+  @JsonProperty("data")
   private List<E> contents;
 
 }

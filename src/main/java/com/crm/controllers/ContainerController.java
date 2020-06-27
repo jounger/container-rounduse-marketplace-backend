1324dd8c765aec2fb38fac068997b01bb9dec661
minhkq1998 <khong.quangminh1@gmail.com>
2020-06-28 02:42:45 +0700

    CRUD Container

--- src/main/java/com/crm/controllers/ContainerController.java
+++ src/main/java/com/crm/controllers/ContainerController.java
@@ -1,19 +1,47 @@
 package com.crm.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins="*", maxAge=3600)
+import com.crm.models.Container;
+import com.crm.models.dto.ContainerDto;
+import com.crm.models.mapper.ContainerMapper;
+import com.crm.payload.request.ContainerRequest;
+import com.crm.payload.request.PaginationRequest;
+import com.crm.payload.response.MessageResponse;
+import com.crm.payload.response.PaginationResponse;
+import com.crm.services.ContainerService;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/container")
 public class ContainerController {
-  /*
+
   @Autowired
   private ContainerService containerService;
-  
+
   @GetMapping("")
-  @PreAuthorize("hasRole('MODERATOR')")
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER') or hasRole('MERCHANT')")
   public ResponseEntity<?> getContainers(@Valid PaginationRequest request) {
     Page<Container> pages = containerService.getContainers(request);
     PaginationResponse<ContainerDto> response = new PaginationResponse<>();
@@ -21,66 +49,97 @@ public class ContainerController {
     response.setPageSize(request.getLimit());
     response.setTotalElements(pages.getTotalElements());
     response.setTotalPages(pages.getTotalPages());
-    
+
     List<Container> containers = pages.getContent();
     List<ContainerDto> containerDto = new ArrayList<>();
     containers.forEach(container -> containerDto.add(ContainerMapper.toContainerDto(container)));
     response.setContents(containerDto);
-    
+
     return ResponseEntity.ok(response);
   }
-  
+
   @GetMapping("/{id}")
-  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER')")
-  public ResponseEntity<?> getContainer(@PathVariable Long id){
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER') or hasRole('MERCHANT')")
+  public ResponseEntity<?> getContainer(@PathVariable Long id) {
     Container container = containerService.getContainerById(id);
     ContainerDto containerDto = new ContainerDto();
     containerDto = ContainerMapper.toContainerDto(container);
     return ResponseEntity.ok(containerDto);
   }
-  
-  @GetMapping("/forwarder/{id}")
-  @PreAuthorize("hasRole('FORWARDER')")
-  public ResponseEntity<?> getContainersByForwarder(@PathVariable Long id, @Valid PaginationRequest request) {
-    
-    Page<Container> pages = containerService.getContainersByForwarder(id, request);
+
+  @GetMapping("/billOfLading/{id}")
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER') or hasRole('MERCHANT')")
+  public ResponseEntity<?> getContainersByBillOfLading(@PathVariable Long id, @Valid PaginationRequest request) {
+
+    Page<Container> pages = containerService.getContainersByBillOfLading(id, request);
     PaginationResponse<ContainerDto> response = new PaginationResponse<>();
     response.setPageNumber(request.getPage());
     response.setPageSize(request.getLimit());
     response.setTotalElements(pages.getTotalElements());
     response.setTotalPages(pages.getTotalPages());
-    
+
     List<Container> containers = pages.getContent();
     List<ContainerDto> containerDto = new ArrayList<>();
     containers.forEach(container -> containerDto.add(ContainerMapper.toContainerDto(container)));
     response.setContents(containerDto);
-    
+
     return ResponseEntity.ok(response);
-    
+
   }
-  
-  @PostMapping("")
-  @PreAuthorize("hasRole('FORWARDER')")
-  public ResponseEntity<?> createContainer(@Valid @RequestBody ContainerRequest request) {
-    containerService.createContainer(request);
-    return ResponseEntity.ok(new MessageResponse("Container has been created successfully"));
+
+  @GetMapping("/inbound/{id}")
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER') or hasRole('MERCHANT')")
+  public ResponseEntity<?> getContainersByInbound(@PathVariable Long id, @Valid PaginationRequest request) {
+
+    Page<Container> pages = containerService.getContainersByInbound(id, request);
+    PaginationResponse<ContainerDto> response = new PaginationResponse<>();
+    response.setPageNumber(request.getPage());
+    response.setPageSize(request.getLimit());
+    response.setTotalElements(pages.getTotalElements());
+    response.setTotalPages(pages.getTotalPages());
+
+    List<Container> containers = pages.getContent();
+    List<ContainerDto> containerDto = new ArrayList<>();
+    containers.forEach(container -> containerDto.add(ContainerMapper.toContainerDto(container)));
+    response.setContents(containerDto);
+
+    return ResponseEntity.ok(response);
+
   }
-  
+
+  @PostMapping("/billOfLading/{id}")
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER')")
+  public ResponseEntity<?> createContainer(@PathVariable Long id, @Valid @RequestBody ContainerRequest request) {
+    Container container = containerService.createContainer(id, request);
+    ContainerDto containerDto = ContainerMapper.toContainerDto(container);
+    return ResponseEntity.ok(containerDto);
+  }
+
   @Transactional
   @PutMapping("")
-  @PreAuthorize("hasRole('FORWARDER')")
-  public ResponseEntity<?> updateContainer(@Valid @RequestBody ContainerRequest request){
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER')")
+//  @PreAuthorize("hasRole('FORWARDER')")
+  public ResponseEntity<?> updateContainer(@Valid @RequestBody ContainerRequest request) {
     Container container = containerService.updateContainer(request);
     ContainerDto containerDto = ContainerMapper.toContainerDto(container);
     return ResponseEntity.ok(containerDto);
   }
-  
+
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER')")
+  @RequestMapping(value = "/{id}", method = RequestMethod.PATCH, consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<?> editContainer(@RequestBody Map<String, Object> updates, @PathVariable("id") Long id) {
+    Container container = containerService.editContainer(updates, id);
+    ContainerDto containerDto = new ContainerDto();
+    containerDto = ContainerMapper.toContainerDto(container);
+    return ResponseEntity.ok(containerDto);
+  }
+
   @Transactional
   @DeleteMapping("/{id}")
-  @PreAuthorize("hasRole('FORWARDER')")
-  public ResponseEntity<?> removeContainer(@PathVariable Long id){       
+  @PreAuthorize("hasRole('MODERATOR') or hasRole('FORWARDER')")
+  public ResponseEntity<?> removeContainer(@PathVariable Long id) {
     containerService.removeContainer(id);
     return ResponseEntity.ok(new MessageResponse("Container has remove successfully"));
   }
-    */
+
 }
--- src/main/java/com/crm/repository/ContainerRepository.java
+++ src/main/java/com/crm/repository/ContainerRepository.java
@@ -1,5 +1,8 @@
 package com.crm.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,8 +18,26 @@ public interface ContainerRepository extends JpaRepository<Container, Long> {
   Boolean existsByContainerNumber(String containerNumber);
 
   Boolean existsByLicensePlate(String licensePlate);
-  
+
   @Query(value = "SELECT b FROM Container b WHERE b.billOfLading.id = :id")
+  Page<Container> getContainersByBillOfLading(@Param("id") Long id, Pageable pageable);
+
+  /*
+   * @param containerType as ContainerType.name
+   * 
+   * @param shippingLine as ShippingLine.companycode
+   * 
+   * @param portOfLoading as PortOfLoading.nameCode
+   */
+  @Query(value = "FROM Container c" + " WHERE c.billOfLading.inbound.shippingLine.companyCode = :shippingLine"
+      + " AND c.billOfLading.inbound.containerType.name = :containerType" + " AND c.status IN :status"
+      + " AND c.billOfLading.inbound.emptyTime < :packingTime" + " AND c.billOfLading.freeTime < :cutOffTime"
+      + " AND c.billOfLading.portOfDelivery.nameCode = :portOfLoading")
+  List<Container> findByOutbound(@Param("shippingLine") String shippingLine,
+      @Param("containerType") String containerType, @Param("status") List<String> status,
+      @Param("packingTime") LocalDateTime packingTime, @Param("cutOffTime") LocalDateTime cutOffTime,
+      @Param("portOfLoading") String portOfLoading);
+
+  @Query(value = "SELECT b FROM Container b WHERE b.billOfLading.inbound.id = :id")
   Page<Container> getContainersByInbound(@Param("id") Long id, Pageable pageable);
-  
 }

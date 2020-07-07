2cc17fa69b23bbf6ee9e38c1c74154c69b8d3d8b
Nguyen Van An <44369558+jounger@users.noreply.github.com>
2020-07-07 17:33:15 +0700

    ADD REST Query Language with Spring Data JPA Specifications (#64)

---
+++ src/main/java/com/crm/specification/UserSpecification.java
@@ -0,0 +1,9 @@
+package com.crm.specification;
+
+import com.crm.models.User;
+
+public class UserSpecification extends GeneticSpecification<User>{
+
+  private static final long serialVersionUID = 1L;
+
+}

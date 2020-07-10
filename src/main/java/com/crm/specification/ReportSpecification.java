1c3243c06eaa9f3df21c5da7b762e4fc0e2b876a
Nguyen Van An <44369558+jounger@users.noreply.github.com>
2020-07-10 10:29:07 +0700

    Revert "Feature bidding management (#68)"
    This reverts commit a1c03dc1458842f1a8d38736e88074169a73c57c.

--- src/main/java/com/crm/specification/ReportSpecification.java
+++
@@ -1,9 +0,0 @@
-package com.crm.specification;
-
-import com.crm.models.Report;
-
-public class ReportSpecification extends GeneticSpecification<Report>{
-
-  private static final long serialVersionUID = 1L;
-
-}

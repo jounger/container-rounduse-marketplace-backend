bc6fb01b29fcc1819931a7ef6d134f1bd5b53a6e
MinQ <nguyenquyenmnek57@gmail.com>
2020-07-28 01:16:44 +0700

    CTREAT UserController, BiddingDocumentController(50%) ID

---
+++ src/test/java/com/crm/controllers/BiddingDocumentControllerIT.java
@@ -0,0 +1,220 @@
+package com.crm.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+
+import com.crm.common.Tool;
+import com.crm.enums.EnumBiddingStatus;
+import com.crm.models.Bid;
+import com.crm.models.BiddingDocument;
+import com.crm.models.Booking;
+import com.crm.models.ContainerType;
+import com.crm.models.Merchant;
+import com.crm.models.Outbound;
+import com.crm.models.Port;
+import com.crm.models.ShippingLine;
+import com.crm.payload.request.BiddingDocumentRequest;
+import com.crm.payload.request.PaginationRequest;
+import com.crm.services.BiddingDocumentService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration
+class BiddingDocumentControllerIT {
+
+  private static final Logger logger = LoggerFactory.getLogger(UserControllerIT.class);
+
+  @Autowired
+  protected MockMvc mockMvc;
+
+  @MockBean
+  private BiddingDocumentService biddingDocumentService;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  PaginationRequest paginationRequest;
+
+  Page<BiddingDocument> pages;
+
+  BiddingDocument biddingDocument;
+
+  List<BiddingDocument> listBidDoc;
+
+  LinkedMultiValueMap<String, String> requestParams;
+
+  LocalDateTime timeNow = LocalDateTime.now();
+
+  Merchant merchant;
+
+  Outbound outbound;
+
+  Booking booking;
+
+  Port port;
+
+  ContainerType containerType;
+
+  ShippingLine shippingLine;
+
+  @BeforeEach
+  public void setUp() {
+    biddingDocument = new BiddingDocument();
+    biddingDocument.setId(1L);
+    biddingDocument.setBidOpening(timeNow);
+    biddingDocument.setBidClosing(timeNow.plusHours(8));
+    biddingDocument.setIsMultipleAward(false);
+    biddingDocument.setBidFloorPrice(2000D);
+    biddingDocument.setPriceLeadership(2000D);
+    biddingDocument.setBidPackagePrice(3000D);
+    biddingDocument.setCurrencyOfPayment("VND");
+    biddingDocument.setStatus(EnumBiddingStatus.BIDDING.name());
+
+    merchant = new Merchant();
+    merchant.setUsername("merchant");
+    biddingDocument.setOfferee(merchant);
+
+    shippingLine = new ShippingLine();
+    shippingLine.setCompanyCode("SPL1");
+
+    port = new Port();
+    port.setNameCode("PortTest");
+
+    containerType = new ContainerType();
+    containerType.setName("CT12");
+
+    booking = new Booking();
+    booking.setBookingNumber("BK123w22");
+    booking.setUnit(3);
+    booking.setCutOffTime(timeNow.plusDays(30));
+    booking.setIsFcl(false);
+    booking.setPortOfLoading(port);
+
+    outbound = new Outbound();
+    outbound.setId(1L);
+    outbound.setBooking(booking);
+    outbound.setContainerType(containerType);
+    outbound.setDeliveryTime(timeNow.plusDays(10));
+    outbound.setGoodsDescription("Abc");
+    outbound.setGrossWeight(1233D);
+    outbound.setPackingStation("Ha Noi");
+    outbound.setPackingTime(timeNow.plusDays(9));
+    outbound.setUnitOfMeasurement("3");
+    outbound.setShippingLine(shippingLine);
+
+    biddingDocument.setOutbound(outbound);
+
+    listBidDoc = new ArrayList<BiddingDocument>();
+    listBidDoc.add(biddingDocument);
+    pages = new PageImpl<BiddingDocument>(listBidDoc);
+
+    requestParams = new LinkedMultiValueMap<>();
+    requestParams.add("page", "0");
+    requestParams.add("limit", "10");
+  }
+
+  @Test
+  @WithMockUser(username = "merchant", roles = { "MERCHANT" })
+  void createBiddingDocument_thenStatusOk_andReturnBiddingDocument() throws Exception {
+    // given
+    BiddingDocumentRequest request = new BiddingDocumentRequest();
+    request.setBidOpening(LocalDateTime.now().toString());
+    when(biddingDocumentService.createBiddingDocument(Mockito.anyString(), Mockito.any(BiddingDocumentRequest.class)))
+        .thenReturn(biddingDocument);
+
+    // when and then
+    MvcResult result = mockMvc
+        .perform(post("/api/bidding-document").contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(objectMapper.writeValueAsString(request)))
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.bidOpening").value(Tool.convertLocalDateTimeToString(timeNow))).andReturn();
+
+    // print response
+    MockHttpServletResponse response = result.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+  }
+
+  @Test
+  @WithMockUser(username = "merchant", roles = { "MERCHANT" })
+  void getBiddingDocument_thenStatusOk_andReturnBiddingDocument() throws Exception {
+    // given
+    when(biddingDocumentService.getBiddingDocument(Mockito.anyLong())).thenReturn(biddingDocument);
+
+    // when and then
+    MvcResult result = mockMvc.perform(get("/api/bidding-document/1").contentType(MediaType.APPLICATION_JSON_VALUE))
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.bidOpening").value(Tool.convertLocalDateTimeToString(timeNow))).andReturn();
+
+    // print response
+    MockHttpServletResponse response = result.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+  }
+
+  @Test
+  @WithMockUser(username = "merchant", roles = { "MERCHANT" })
+  void getBiddingDocuments_thenStatusOk_andReturnBiddingDocuments() throws Exception {
+    // given
+    LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+    requestParams.add("page", "0");
+    requestParams.add("limit", "10");
+    when(biddingDocumentService.getBiddingDocuments(Mockito.anyString(), Mockito.any(PaginationRequest.class)))
+        .thenReturn(pages);
+
+    // when and then
+    MvcResult result = mockMvc
+        .perform(get("/api/bidding-document").contentType(MediaType.APPLICATION_JSON_VALUE).params(requestParams))
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.data[0].id").value(1))
+        .andExpect(jsonPath("$.data[0].merchant").value("merchant")).andReturn();
+
+    // print response
+    MockHttpServletResponse response = result.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+  }
+
+  @Test
+  @WithMockUser(username = "merchant", roles = { "MERCHANT" })
+  void getBiddingDocumentByBid_thenStatusOk_andReturnBiddingDocument() throws Exception {
+    // given
+    when(biddingDocumentService.getBiddingDocumentByBid(Mockito.anyLong(), Mockito.anyString()))
+        .thenReturn(biddingDocument);
+
+    // when and then
+    MvcResult result = mockMvc
+        .perform(get("/api/bidding-document/bid/1").contentType(MediaType.APPLICATION_JSON_VALUE).params(requestParams))
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$.merchant").value("merchant")).andReturn();
+
+    // print response
+    MockHttpServletResponse response = result.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+  }
+
+}
--- src/test/java/com/crm/controllers/UserControllerIT.java
+++ src/test/java/com/crm/controllers/UserControllerIT.java
@@ -7,7 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -15,129 +14,106 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.crm.models.Role;
 import com.crm.models.User;
 import com.crm.payload.request.PaginationRequest;
-import com.crm.repository.UserRepository;
-import com.crm.security.jwt.AuthEntryPointJwt;
-import com.crm.security.jwt.JwtUntils;
-import com.crm.security.services.UserDetailsImpl;
-import com.crm.security.services.UserDetailsServiceImpl;
-import com.crm.services.impl.UserServiceImpl;
+import com.crm.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@AutoConfigureMockMvc(addFilters = false)
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = UserController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration
 public class UserControllerIT {
 
+  private static final Logger logger = LoggerFactory.getLogger(UserControllerIT.class);
+
   @Autowired
-  private MockMvc mockMvc;
+  protected MockMvc mockMvc;
+
+  @MockBean
+  private UserService userService;
 
   @Autowired
   private ObjectMapper objectMapper;
 
-  @MockBean
-  private UserServiceImpl userServiceImpl;
+  PaginationRequest paginationRequest;
 
-  @MockBean
-  private UserDetailsServiceImpl userDetailsService;
-
-  @MockBean
-  private AuthenticationManager authenticationManager;
-
-  @MockBean
-  private JwtUntils jwtUntils;
-
-  @MockBean
-  private AuthEntryPointJwt authEntryPointJwt;
-
-  @MockBean
-  private UserRepository userRepository;
-
-  @MockBean
-  private PasswordEncoder passwordEncoder;
-
-  User user;
-
-  private PaginationRequest request;
-
-  Role role;
+  List<User> users = new ArrayList<>();
 
   Page<User> pages;
 
-  @Autowired
-  WebApplicationContext ctx;
-
-  @Autowired
-  private FilterChainProxy springSecurityFilterChain;
-
-  String idToken;
+  User user;
 
   @BeforeEach
   public void setUp() {
-    System.out.println("------------------");
+    
+    logger.info("------------------------------------");
+    paginationRequest = new PaginationRequest();
+    paginationRequest.setPage(0);
+    paginationRequest.setLimit(10);
+
+    // CREATE USER
     user = new User();
     user.setId(1L);
-    user.setUsername("moderator");
-    user.setPassword(passwordEncoder.encode("123456"));
-    user.setPhone("0967390001");
-    user.setEmail("anvannguyen@gmail.com");
-    user.setAddress("Ha Noi, Viet Nam");
-    user.setStatus("ACCEPTED");
-    List<User> users = new ArrayList<User>();
-    users.add(user);
-    role = new Role();
+    user.setUsername("nguyenvanan");
+    user.setPassword("123456");
+    user.setPhone("0967390098");
+    user.setEmail("annvse@fpt.edu.vn");
+    user.setAddress("HN, Vietnam");
+    Collection<Role> roles = new ArrayList<Role>();
+    Role role = new Role();
     role.setId(1L);
-    role.setName("ROLE_MODERATOR");
-    user.getRoles().add(role);
+    role.setName("ROLE_FORWARDER");
+    roles.add(role);
+    user.setRoles(roles);
+    users.add(user);
     pages = new PageImpl<User>(users);
-    request = new PaginationRequest();
-    request.setPage(0);
-    request.setLimit(10);
-
-    mockMvc = MockMvcBuilders.webAppContextSetup(ctx).addFilters(springSecurityFilterChain).build();
-    // #formatter:off
-    UserDetailsServiceImpl userDetailsServiceImpl = null;
-    userDetailsServiceImpl = ctx.getBean(UserDetailsServiceImpl.class);
-    final Authentication authentication = authenticationManager
-        .authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    idToken = jwtUntils.generateJwtToken(authentication);
-    System.out.println("Id token: " + idToken);
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    System.out.println("principal:" + SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-
   }
 
   @Test
-  void searchUsersByUsername() throws Exception {
+  @WithMockUser(username = "moderator", roles = { "MODERATOR" })
+  public void whenGetUsers_then200() throws Exception {
+
+    // given
+    LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
+    requestParams.add("page", "0");
+    requestParams.add("limit", "10");
+
+    when(userService.getUsers((Mockito.any(PaginationRequest.class)))).thenReturn(pages);
+
+    // when and then
+    MvcResult mvcResult = mockMvc
+        .perform(get("/api/user").params(requestParams).contentType(MediaType.APPLICATION_JSON_VALUE)).andDo(print())
+        .andExpect(status().isOk()).andExpect(jsonPath("$.page").value(0))
+        .andExpect(jsonPath("$.totalElements").value(1)).andExpect(jsonPath("$.totalPages").value(1))
+        .andReturn();
+
+    // print response
+    MockHttpServletResponse response = mvcResult.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+  }
+
+  @Test
+  @WithMockUser(username = "moderator", roles = { "MODERATOR" })
+  void searchUsersByUsername_thenStatusOkAndReturnUsers() throws Exception {
     String search = "username:moderator";
 
     LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
@@ -145,55 +121,36 @@ public class UserControllerIT {
     requestParams.add("limit", "10");
     requestParams.add("search", search);
 
-    when(userServiceImpl.searchUsers(Mockito.any(PaginationRequest.class), Mockito.anyString())).thenReturn(pages);
+    when(userService.searchUsers(Mockito.any(PaginationRequest.class), Mockito.anyString())).thenReturn(pages);
     MvcResult mvcResult = mockMvc
         .perform(get("/api/user/filter").contentType(MediaType.APPLICATION_JSON).params(requestParams)
             .accept(MediaType.APPLICATION_JSON))
         .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.totalPages").isNumber())
         .andExpect(jsonPath("$.totalElements").value(1)).andExpect(jsonPath("$.data[0].id").value(1)).andReturn();
-    String actualResponseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-    String expectedResponseBody = objectMapper.writeValueAsString(request);
-    System.out.println("actual: " + actualResponseBody);
-    System.out.println("expected: " + expectedResponseBody);
+
+    // RESPONSE
+    MockHttpServletResponse response = mvcResult.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
+    
   }
 
   @Test
-  @WithMockUser(username = "admin", authorities = { "MODERATOR" })
-  void getUsers() throws Exception {
-
-    LinkedMultiValueMap<String, String> requestParams = new LinkedMultiValueMap<>();
-    requestParams.add("page", "0");
-    requestParams.add("limit", "10");
-    requestParams.add("status", "PENDING");
-
-    when(userServiceImpl.getUsers(Mockito.any(PaginationRequest.class))).thenReturn(pages);
-    mockMvc
-        .perform(get("/api/user").contentType(MediaType.APPLICATION_JSON).params(requestParams)
-            .accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk()).andExpect(jsonPath("$.totalElements").value(1))
-        .andExpect(jsonPath("$.data[0].status").value("PENDING")).andReturn();
-  }
-
-  @Test
+  @WithMockUser(username = "moderator", roles = { "MODERATOR" })
   void changeStatus() throws Exception {
     Map<String, Object> updates = new HashMap<>();
     updates.put("status", "ACCEPTED");
+    user.setStatus("ACCEPTED");
 
-    when(userServiceImpl.changeStatus(Mockito.anyLong(), Mockito.anyMap())).thenReturn(user);
-    String token = "Bearer " + idToken;
+    when(userService.changeStatus(Mockito.anyLong(), Mockito.anyMap())).thenReturn(user);
+    when(userService.searchUsers(Mockito.any(PaginationRequest.class), Mockito.anyString())).thenReturn(pages);
     MvcResult mvcResult = mockMvc
-        .perform(patch("/api/user/1").contentType(MediaType.APPLICATION_JSON).header("Authorization", token)
+        .perform(patch("/api/user/1").contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(updates)).accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk()).andReturn();
-    System.out.println("------------");
-    System.out.println(mvcResult.getResponse().getRedirectedUrl());
-    System.out.println(mvcResult.getResponse().getIncludedUrl());
-    System.out.println(mvcResult.getResponse().getForwardedUrl());
-    String actualResponseBody = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
-    String expectedResponseBody = objectMapper.writeValueAsString(request);
-    System.out.println("actual: " + actualResponseBody);
-    System.out.println("expected: " + expectedResponseBody);
+        .andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.id").value(1))
+        .andExpect(jsonPath("$..status").value("ACCEPTED")).andReturn();
 
+    // RESPONSE
+    MockHttpServletResponse response = mvcResult.getResponse();
+    logger.info("Reponse: {}", response.getContentAsString());
   }
-
 }

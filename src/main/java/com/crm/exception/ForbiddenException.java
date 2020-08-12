616fdc8fcdf6461151ddf6e621ebfc0e9fb43b64
Nguyen Van An <nguyenvanan.12a8@gmail.com>
2020-08-12 19:21:01 +0700

    CREATE Upload file module

--- src/main/java/com/crm/exception/ForbiddenException.java
+++ src/main/java/com/crm/exception/ForbiddenException.java
@@ -1,9 +1,9 @@
 package com.crm.exception;
 
-public class ForbiddenException extends RuntimeException{
+public class ForbiddenException extends RuntimeException {
 
   private static final long serialVersionUID = 1L;
-  
+
   public ForbiddenException(String message) {
     super(message);
   }

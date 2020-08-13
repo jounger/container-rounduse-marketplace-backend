0a656c0bad85002423a1b7fbe01618655d8d2d5d
Van Quyen Nguyen <42163214+MinQNo1@users.noreply.github.com>
2020-08-13 22:56:27 +0700

    Feature bidding management (#117)
    * CREATE UPDATE Inbound, BillOfLading And Container
    
    * REMOVE comment
    
    * FIX conflict
    
    * COMMIT for fix other branch
    
    * CRUD bid and biddingDocument
    
    * COMMIT after test
    
    * FIX conflict when merge master
    
    * EDIT logic
    
    * EDIT Logic
    
    * REMOVE update password
    
    * UPDATE Logic
    
    * UPDATE logic
    
    * EDIT function in Reposotories and ADD Bid logic
    
    * EDIT Convention
    
    * EDIT get Combined by User logic
    
    * UPDATE creat, update and edit bid logic
    
    * UPDATE create, update and edit bid logic
    
    * UPDATE biddoc and bid logic
    
    * CRUD Contract, Evidence, Payment, Update Bid Logic
    
    * CRUD Feedback and Report
    
    * DELETE file redundancy
    
    * Evidence impl abtract method
    
    * Combined and contract update
    
    * UPDATE combined and contract logic
    
    * UPDATE Combined Logic
    
    * FIX Combined and Feedback Logic
    
    * ADD search Supplier, CREATE Edit feedback
    
    * Fix duplicate BidDocRepo
    
    * Add supplier rating value when create
    
    * ADD GET Bid, BidDoc, Evidence, Payment
    
    * Fix Bid, BidDoc, Feedback and Report logic
    
    * update bidDoc logic
    
    * CRUD Rating And REFACTOR edit with Tool.equal
    
    * EDIT user update information, remove unuse repository
    
    * remove unuse repository
    
    * UPDATE Combined Logic
    
    * REFACTOR Error message, UPDATE combined logic
    
    * UPDATE combined logic
    
    * ADD get Operator by username, get Report by id at Controller
    
    * ADD send date
    
    * REFACTOR UserDetailsImpl to UserDetails
    
    * update getBiddingDocumentByExistsCombined
    
    * REFACTOR All services create, edit
    
    * ADD update bid container, supply code
    
    * resolve conflict
    
    * REFACTOR update bid container
    
    * CRUD shippingInfo
    
    * update getBid
    
    * REFACTOR create Combined logic
    
    * ADD getShippingInfo
    
    * REFACTOR ForbiddenException, ADD getShippingInfosByCombined, getContractByCombined in Controllers
    
    * EDIT BillOfLading and Booking logic
    
    * EDIT create Combined
    
    * EDIT create combined, REFACTOR containerNumber -> number
    
    * EDIT Logic of '||' and ADD getBiddingDocumentByCombined
    
    * REFACTOR message response
    
    * REFACTOR UML diagram
    
    * CREATE ResetPassword Module
    
    * EDIT getBidByBiddingDocument
    
    Co-authored-by: minhkq1998 <khong.quangminh1@gmail.com>
    Co-authored-by: Nguyen Van An <nguyenvanan.12a8@gmail.com>
    Co-authored-by: Nguyen Van An <44369558+jounger@users.noreply.github.com>

--- src/main/java/com/crm/repository/BidRepository.java
+++ src/main/java/com/crm/repository/BidRepository.java
@@ -1,7 +1,5 @@
 package com.crm.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,8 +12,9 @@ import com.crm.models.Bid;
 @Repository
 public interface BidRepository extends JpaRepository<Bid, Long> {
 
-  @Query(value = "SELECT b FROM Bid b WHERE b.biddingDocument.id = :id")
-  Page<Bid> findByBiddingDocument(@Param("id") Long id, Pageable pageable);
+  @Query(value = "FROM Bid b LEFT JOIN b.biddingDocument bd WHERE bd.id = :id "
+      + "AND (b.bidder.username = :username OR bd.offeree.username = :username)")
+  Page<Bid> findByBiddingDocument(@Param("id") Long id, String username, Pageable pageable);
 
   @Query(value = "SELECT b FROM Bid b LEFT JOIN b.biddingDocument bd WHERE bd.id = :id "
       + "AND (bd.offeree.username = :username or b.bidder.username = :username) AND b.combined IS NOT NULL")
@@ -27,9 +26,6 @@ public interface BidRepository extends JpaRepository<Bid, Long> {
 
   @Query(value = "SELECT b FROM Bid b WHERE b.bidder.username = :username AND b.status = :status")
   Page<Bid> findByForwarder(@Param("username") String username, @Param("status") String status, Pageable pageable);
-
-  @Query(value = "FROM Bid b WHERE b.biddingDocument.id = :id AND b.bidder.username = :username")
-  Optional<Bid> findByBiddingDocumentAndForwarder(@Param("id") Long id, @Param("username") String username);
 
   @Query(value = "SELECT CASE WHEN COUNT(b) = 0 THEN TRUE ELSE FALSE END "
       + "FROM Bid b JOIN b.containers c WHERE b.biddingDocument.id = :id AND c.status != 'DELIVERED'")
